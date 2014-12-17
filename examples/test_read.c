1abcd9ec598ef498578f7877f62f5f6cb36eccec
relmachhour <relmachhour@spartsystems.com>
2014-12-17 17:42:57 +0000

    Update test_read.c

--- examples/test_read.c
+++ examples/test_read.c
@@ -7,9 +7,9 @@ int main(int argc, char * argv [])
 {
 	s_serial_t *s;
 	int baudr=9600,n,i;
-  char buf[4096];
-  printf("%s\n",argv[1] );
-  char * comport=argv[1];
+	char buf[4096];
+	printf("%s\n",argv[1] );
+	char * comport=argv[1];
   
 
 
@@ -23,10 +23,9 @@ int main(int argc, char * argv [])
 
 	s_serial_set_baudrate(s,baudr);
 
-	  while(1)
- 	 {        
-
-  		 n = s_serial_read(s, buf, 4095);
+	while(1)
+ 	{
+ 		n = s_serial_read(s, buf, 4095);
    		if(n > 0)
     		{
     		  buf[n] = 0;   /* always put a "null" at the end of a string! */
@@ -39,16 +38,15 @@ int main(int argc, char * argv [])
    		   	  }
    		  	 }
 
-      printf("received %i bytes: %s\n", n, (char *)buf);
+      	printf("received %i bytes: %s\n", n, (char *)buf);
     }
     #ifdef _WIN32
       Sleep(1000);
     #else
       usleep(1000000);  /* sleep for 1 Second */
-      #endif
+    #endif
 
   }
-
-	printf("%d\n",s->baudr );
+      printf("%d\n",s->baudr );
     return 0;
 }
